0e8556f5cf64e16ead426cb2cea4bd6838c84764
YtecSolutions <ytec@outlook.com.br>
2023-10-05 16:08:00 -0300

    console.log arrays

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -12,3 +12,27 @@ Lista de dados não indexados e formados por propriedades e valores.
 
 let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
 
+console.log(cursos); //saída direta/estruturada
+
+console.log(cursos[2]);
+
+console.log(`O curso é ${cursos[0]}`); //SQL
+
+// Tipos de dados mais comuns no JS
+
+
+/* String (textos em geral) 
+Number ( números  inteiros, decimais, octais, etc
+    Array (vetor/matriz - listas de dados indexados)
+    Object (dados não indexados com propriedades e valores )
+    Boolean (Booleano, valores " true/verdadeiro" ou False/falso") 
+    usar o camndo typeof nomeDaVariavel"  
+    */
+
+  
+
+
+
+
+
+

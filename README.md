d47757d58b531c47320fb2e9181964f30c68028a
YtecSolutions <ytec@outlook.com.br>
2023-10-05 15:03:43 -0300

    Console log e variáveis

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # aulas-javascript
  Atividades com a linguagem JavaScript
+### Sobre a linguagem 
+
+- JavaScript não é Java! 
+- Abreviatura JS
+- É uma linguagem voltada principalmente para o Front-End
+- Mas também pode ser usado no Back-end
+- Mas também pode ser usado no Back-End através do Node.js
+- JS pode ser usado para criar recursos interativos nos sites, como por exemplo:
+ - Galeria de fotos
+ - Carousel/Slider de conteúdo
+ - funcionalidades de navegação
+ - Validações mais avançadas de formulário
+ - JS pode ser usado até mesmo para criação de aplicativos de games.
+
+ 
---
+++ js/sintaxe.js
@@ -0,0 +1,30 @@
+console.log("Programado Web");
+console.log("Senac Penha");
+/* Comentário em bloco */
+// Comentário simples
+
+// Manipulação de dados
+
+//Variáveis 
+let curso = " HTML e CSS3";
+let ano = 2023; 
+// Constante
+const mediaMinima = 7;
+// Saída de dados a partir da memória
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+// Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023
+
+// Template String/literal: saída usando crases e ${}
+console.log (`Estou fazendo o curso de ${curso} neste ano de ${ano}.`) ;   
+
+// Concatenação
+console.log("Estou fazendo o curso de "  + curso+ " neste ano de " + ano+".");
+
+
+
+
+
+
+

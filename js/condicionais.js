1dda9c9941f4363fe1e98a2ba937a70d6c2d97da
YtecSolutions <ytec@outlook.com.br>
2023-10-10 15:15:57 -0300

    Condicionais

---
+++ js/condicionais.js
@@ -0,0 +1,26 @@
+//Estruturas de Controle Condicional
+// Comandos que permitem analisar uma ou mais condições.
+/* E a partir do resultado desta análise(verdadeiro ou falso),
+o programa pode executar ações diferentes. 
+
+Comandos mais comuns: if(Se), else(senão), else if(senão se)
+let usuario= "Juean";
+*/
+
+let usuario = "Juean";
+let idade = 35;
+
+//Verificar idade e determinar se é menor ou maior.
+
+// Condicional Composta
+
+if (idade >= 18) {
+
+    console.log(`${usuario} é maior de idade!`);
+} else {
+    console.log(`${usuario} é menor de idade!`);
+
+}
+ 
+
+

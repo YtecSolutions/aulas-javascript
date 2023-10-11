1b92d0e548ad5d6b3365a317d8873195d253c997
YtecSolutions <ytec@outlook.com.br>
2023-10-11 14:22:15 -0300

    Operadores &&

---
+++ js/04-operadores-logicos.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+//Resultados do aluno fictício
+
+let aluno = "Jean";
+let mediaFinal = 10;
+let faltas = 10;
+let resultado;
+
+/* Operador lógico E (AND) */
+if (mediaFinal >= mediaMinima && faltas <= limiteDeFaltas) {
+    resultado = "aprovado";
+} else {
+    resultado = "reprovado";
+}
+console.log(`O aluno ${aluno} está ${resultado}`);
+console.log(`Média do aluno: ${mediaFinal}`);
+console.log(`Faltas do aluno:${faltas}`);

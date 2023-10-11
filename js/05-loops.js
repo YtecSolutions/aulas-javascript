6ac2bb30eafe8c4b4098467168cfeddb966f24e4
YtecSolutions <ytec@outlook.com.br>
2023-10-11 16:03:46 -0300

    Comando While

---
+++ js/05-loops.js
@@ -0,0 +1,23 @@
+/* Comandos de Repetição
+Instruções para realizar ações
+por uma quantidade específica de vezes.
+
+Comandos Tradicionais:
+- while     -> ENQUANTO
+-do/while   -> FAÇA/ENQUANTO
+- for       -> PARA 
+
+
+// Obs: normalmente os comandos são controlados por meio de uma variável contadora;
+
+// Exemplo 1: while 
+// Nomes comuns para esta variável:
+// i, j, k 
+
+*/
+let contador = 1;
+while(contador <= 500){
+    console.log(`Valor de contador: ${contador}`);
+    contador++;
+}
+

5d117f70e7269bc5fa2fbb513fe25adbdb405f4f
YtecSolutions <ytec@outlook.com.br>
2023-10-17 14:53:46 -0300

    Funções -definição

---
+++ js/06-funcoes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Sobre Funções
+
+/* Funções são blocos de código que podem ser executados e reaproveitados em praticamente qualquer lugar de aplicação/site */
+
+// Usar funções também é benéfico no ponto de vista de organização de código. 
+
+// O JS já possui centaenas de funções prontas:
+
+/* Exemplos: log(), alert(), prompt(), toFixed etc... */ 
+
